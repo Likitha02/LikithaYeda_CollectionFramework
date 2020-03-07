ba433df327b3e6fbf706653073d8a020a25260e9
Likitha <177r1a04b8@gmail.com>
2020-03-07 07:08:13 +0530

    commit 1

---
+++ Collection/src/test/java/com/epam/Collection/Custom.java
@@ -0,0 +1,24 @@
+package com.epam.Collection;
+import java.util.*;
+public class Custom
+{
+  int b=0;
+  int element[]=new int[10000];
+  ArrayList al=new ArrayList();
+  public void add(int a) {
+	  al.add(a);	  
+  }
+  public void fetch(int a)
+  {
+	  System.out.println(al.get(a));
+  }
+  public void remove(int a)
+  {
+	  al.remove(a);
+  }
+  public void print()
+  {
+	  System.out.println(al);
+  }
+ 
+}
